53548050c3cc9595e88e6c9ae92917b13a3b4a76
autyan <autyan@hotmail.com>
2016-03-23 00:56:56 +0800

    创建项目

---
+++ MisakaBanZai/Views/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MisakaBanZai.Views
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
